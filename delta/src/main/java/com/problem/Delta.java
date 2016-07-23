b1a46f5cbada7e1140a12d218ea63e662275115a
arbitrary-dev <arbitrary-dev@users.noreply.github.com>
2016-07-23 18:51:25 +0500

    fix indentation

--- delta/src/main/java/com/problem/Delta.java
+++ delta/src/main/java/com/problem/Delta.java
@@ -5,34 +5,34 @@ import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Delta {
-    
-    public static void solve(InputStream in, PrintStream out) {
-    	Scanner sc = new Scanner(in);
-    	
-    	try {
-	    	if (!sc.hasNext()) return;
-	    	
-	    	int prev = sc.nextInt();
-	    	out.printf("%d ", prev); // the first element reproduced as-is
-	    	
-	    	// Assume input numbers are all positive.
-    		for (int i = sc.nextInt(); ; prev = i, i = sc.nextInt()) {
-    			int delta = i - prev;
-    			
-    			if (Math.abs(delta) > 127)
-    				out.print("-128 ");
-    			
+
+	public static void solve(InputStream in, PrintStream out) {
+		Scanner sc = new Scanner(in);
+
+		try {
+			if (!sc.hasNext()) return;
+
+			int prev = sc.nextInt();
+			out.printf("%d ", prev); // the first element reproduced as-is
+
+			// Assume input numbers are all positive.
+			for (int i = sc.nextInt(); ; prev = i, i = sc.nextInt()) {
+				int delta = i - prev;
+
+				if (Math.abs(delta) > 127)
+					out.print("-128 ");
+
 				out.printf("%d ", delta);
-				
+
 				if (!sc.hasNext()) break; // can't check in loop declaration
-    		}
-    	}
-    	finally {
-	    	sc.close();
-    	}
-    }
-    
-    public static void main(String[] args) {
-    	solve(System.in, System.out);
+			}
+		}
+		finally {
+			sc.close();
+		}
+	}
+
+	public static void main(String[] args) {
+		solve(System.in, System.out);
 	}
 }
--- hotel/src/main/java/com/problem/Hotel.java
+++ hotel/src/main/java/com/problem/Hotel.java
@@ -17,21 +17,21 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class Hotel {
-    private static class HotelScore implements Comparable<HotelScore> {
-    	final Integer id;
-    	int score;
-    	
-    	public HotelScore(Integer id) {
-    		this.id = id;
-    	}
-    	
+	private static class HotelScore implements Comparable<HotelScore> {
+		final Integer id;
+		int score;
+
+		public HotelScore(Integer id) {
+			this.id = id;
+		}
+
 		public void scoreReview(String r, Set<String> words) {
 			List<String> rwords = new ArrayList<String>();
 			readWords(r, rwords);
-    		for (String rw : rwords)
-    			if (words.contains(rw))
-    				++score;
-    	}
+			for (String rw : rwords)
+				if (words.contains(rw))
+					++score;
+		}
 
 		@Override
 		public String toString() {
@@ -44,50 +44,50 @@ public class Hotel {
 			if (res != 0) return res;
 			return id - o.id;
 		}
-    }
+	}
 
 	private static void readWords(String from, Collection<String> to) {
-    	BreakIterator bi = BreakIterator.getWordInstance(Locale.US);
-    	bi.setText(from);
-    	
-    	if (bi.first() == DONE) return;
+		BreakIterator bi = BreakIterator.getWordInstance(Locale.US);
+		bi.setText(from);
 
-    	for (int first = bi.first(), last = bi.next(); last != DONE; first = last, last = bi.next())
-    		if (Character.isLetter(from.charAt(first)))
-    			to.add(from.substring(first, last).toLowerCase(Locale.US));
+		if (bi.first() == DONE) return;
+
+		for (int first = bi.first(), last = bi.next(); last != DONE; first = last, last = bi.next())
+			if (Character.isLetter(from.charAt(first)))
+				to.add(from.substring(first, last).toLowerCase(Locale.US));
 	}
-    
-    public static void solve(InputStream in, PrintStream out) {
-    	Scanner sc = new Scanner(in);
-    	
-    	try {
-	    	if (!sc.hasNext()) return;
-	    	
-	    	Set<String> words = new HashSet<>();
-	    	readWords(sc.nextLine(), words);
-	    	int M = sc.nextInt();
-	    	
-	    	Map<Integer, HotelScore> hotels = new HashMap<>();
-	    	for (int i = 0; i < M; ++i) {
-	    		Integer id = sc.nextInt();
-	    		sc.nextLine(); // skip to next line
-	    		
-	    		HotelScore h = hotels.get(id);
-	    		if (h == null) hotels.put(id, h = new HotelScore(id));
-	    		
-	    		h.scoreReview(sc.nextLine(), words);
-	    	}
 
-	    	Set<HotelScore> res = new TreeSet<>();
-	    	for (HotelScore h : hotels.values()) res.add(h);
-	    	for (HotelScore h : res) out.printf("%s ", h);
-    	}
-    	finally {
-	    	sc.close();
-    	}
-    }
-    
-    public static void main(String[] args) {
-    	solve(System.in, System.out);
+	public static void solve(InputStream in, PrintStream out) {
+		Scanner sc = new Scanner(in);
+
+		try {
+			if (!sc.hasNext()) return;
+
+			Set<String> words = new HashSet<>();
+			readWords(sc.nextLine(), words);
+			int M = sc.nextInt();
+
+			Map<Integer, HotelScore> hotels = new HashMap<>();
+			for (int i = 0; i < M; ++i) {
+				Integer id = sc.nextInt();
+				sc.nextLine(); // skip to next line
+
+				HotelScore h = hotels.get(id);
+				if (h == null) hotels.put(id, h = new HotelScore(id));
+
+				h.scoreReview(sc.nextLine(), words);
+			}
+
+			Set<HotelScore> res = new TreeSet<>();
+			for (HotelScore h : hotels.values()) res.add(h);
+			for (HotelScore h : res) out.printf("%s ", h);
+		}
+		finally {
+			sc.close();
+		}
+	}
+
+	public static void main(String[] args) {
+		solve(System.in, System.out);
 	}
 }
