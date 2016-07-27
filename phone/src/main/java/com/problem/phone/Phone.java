9ebad2036eed7edb17404881dcfca398f5528873
arbitrary-dev <arbitrary-dev@users.noreply.github.com>
2016-07-27 14:03:35 +0500

    fix

--- phone/src/main/java/com/problem/phone/Phone.java
+++ phone/src/main/java/com/problem/phone/Phone.java
@@ -3,23 +3,23 @@ package com.problem.phone;
 public class Phone {
     public static String solution(String input) {
     	StringBuilder sb = new StringBuilder();
-    	
+
     	for (char c : input.toCharArray())
 	    	if (Character.isDigit(c))
 	    		sb.append(c);
-        
+
     	int size = 0; // block size
     	for (int i = 0; i < sb.length() - 1; ++i)
     		if (++size == 3) {
     			if (sb.length() - 1 - i == 1) {
-	    			sb.insert(i	, '-');
+	    			sb.insert(i, '-');
 	    			break;
     			} else {
 	    			sb.insert(++i, '-');
 	    			size = 0;
     			}
     		}
-    	
+
         return sb.toString();
     }
 }
