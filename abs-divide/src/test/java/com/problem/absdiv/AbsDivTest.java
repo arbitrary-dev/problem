02dfbe239bb6f47bf39ba2d245e6a743888e2ea2
arbitrary-dev <arbitrary-dev@users.noreply.github.com>
2017-01-10 23:50:33 +0500

    rfct

--- abs-divide/src/test/java/com/problem/absdiv/AbsDivTest.java
+++ abs-divide/src/test/java/com/problem/absdiv/AbsDivTest.java
@@ -1,16 +1,16 @@
 package com.problem.absdiv;
 
-import org.junit.Test;
-
 import static com.problem.absdiv.AbsDiv.solution;
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 public class AbsDivTest {
 	@Test
 	public void test0() {
 		assertEquals(0, solution(6, 6));
 	}
-	
+
 	@Test
 	public void test4() {
 		assertEquals(4, solution(4, 3, 2, 5, 1, 1));
@@ -20,7 +20,7 @@ public class AbsDivTest {
 	public void test6() {
 		assertEquals(6, solution(1, 3, -3));
 	}
-	
+
 	@Test
 	public void test12() {
 		assertEquals(12, solution(-6, 6));
