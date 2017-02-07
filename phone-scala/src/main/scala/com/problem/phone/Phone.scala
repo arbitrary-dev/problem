8574328d3261be45bb04d5da9456d81aecd45a21
arbitrary-dev <arbitrary-dev@users.noreply.github.com>
2017-02-07 22:38:06 +0500

    rfct

--- phone-scala/src/main/scala/com/problem/phone/Phone.scala
+++ phone-scala/src/main/scala/com/problem/phone/Phone.scala
@@ -7,19 +7,17 @@ object Phone {
     input
       .filter(Character.isDigit)
       .grouped(3)
-      .foreach(
-        group => {
-          if (!res.isEmpty) {
-            if (group.length > 1)
-              res += '-'
-            else // corner case for two 2-sized blocks
-              res.insert(res.length - 1, '-')
-          }
-
-          res ++= group
+      .foreach { group =>
+        if (!res.isEmpty) {
+          if (group.length > 1)
+            res += '-'
+          else // corner case for two 2-sized blocks
+            res.insert(res.length - 1, '-')
         }
-      )
 
-    res toString
+        res ++= group
+      }
+
+    res.toString
   }
 }
